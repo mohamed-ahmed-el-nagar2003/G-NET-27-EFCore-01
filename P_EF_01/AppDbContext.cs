8e1047e2db0ff929a348b37e75c8ad5d40f08942
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-22 13:11:25 +0200

    Add project files.

---
+++ P_EF_01/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_EF_01
+{
+    internal class AppDbContext : DbContext
+    {
+
+    }
+}
---
+++ P_EF_01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P_EF_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
